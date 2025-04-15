f814bc4a4fe8a1bbeb683c0fad1e894858d765f0
vhorielova <horielova.viktoria@gmail.com>
2025-04-15 16:15:22 +0300

    create FrontController servlet

---
+++ src/main/java/org/periodicalswebapp/servlets/FrontController.java
@@ -0,0 +1,27 @@
+package org.periodicalswebapp.servlets;
+
+import jakarta.servlet.http.*;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class FrontController extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+    public void destroy() {
+    }
+}

b92b396295cbf3bc0282e08f9944cdc627ded31d
vhorielova <horielova.viktoria@gmail.com>
2025-04-23 20:35:10 +0300

    create LoginFilter

---
+++ src/main/java/org/periodicalswebapp/filters/LoginFilter.java
@@ -0,0 +1,42 @@
+package org.periodicalswebapp.filters;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class LoginFilter implements Filter{
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+        System.out.println("Preprocessing for filter");
+
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+
+        String URI = String.valueOf(req.getServletPath());
+        boolean isLoggedIn = req.getSession().getAttribute("user") != null;
+
+        if(isLoggedIn || URI.equals("/login") || URI.equals("/register")) {
+            chain.doFilter(request, response);
+        }
+        else {
+            res.sendRedirect("/login");
+        }
+
+
+        System.out.println("PostProcessing for filter");
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
+}

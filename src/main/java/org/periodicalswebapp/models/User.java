a481eec8afa0a69009a8b78446e3718fc124da7e
vhorielova <horielova.viktoria@gmail.com>
2025-04-23 20:40:14 +0300

    add User attributes

--- src/main/java/org/periodicalswebapp/models/User.java
+++ src/main/java/org/periodicalswebapp/models/User.java
@@ -3,15 +3,23 @@ package org.periodicalswebapp.models;
 public class User {
     private int id;
     private String name;
+    private String lastname;
+    private String address;
+    private String index;
     private String email;
+    private String password;
 
     public User(){
     }
 
-    public User(int id, String name, String email) {
-        this.id = id;
+    public User(String name, String lastname, String address, String index, String email, String password) {
+        //this.id = id;
         this.name = name;
+        this.lastname = lastname;
+        this.address = address;
+        this.index = index;
         this.email = email;
+        this.password = password;
     }
 
     public int getId() {
@@ -30,11 +38,43 @@ public class User {
         this.name = name;
     }
 
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
     public String getEmail() {
         return email;
     }
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getIndex() {
+        return index;
+    }
+
+    public void setIndex(String index) {
+        this.index = index;
     }
 }

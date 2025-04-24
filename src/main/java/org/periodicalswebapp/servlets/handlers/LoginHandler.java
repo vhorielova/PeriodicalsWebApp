6f30c004bb3988a41e6228882590460cef0fa3c6
vhorielova <horielova.viktoria@gmail.com>
2025-04-24 17:04:19 +0300

    add log in with DAO pattern

--- src/main/java/org/periodicalswebapp/servlets/handlers/LoginHandler.java
+++ src/main/java/org/periodicalswebapp/servlets/handlers/LoginHandler.java
@@ -4,10 +4,15 @@ import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.periodicalswebapp.daoimpl.UserDaoImpl;
+import org.periodicalswebapp.models.User;
 
 import java.io.IOException;
 
 public class LoginHandler implements Handler{
+
+    private final UserDaoImpl userDao = UserDaoImpl.getInstance();
+
     public void handleGet (HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         System.out.println(request.getRequestURI()+" Get");
 
@@ -17,5 +22,19 @@ public class LoginHandler implements Handler{
     }
     public void handlePost (HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         System.out.println(request.getRequestURI()+" Post");
+
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+
+        User user = userDao.getUserByEmail(email);
+
+        if(user != null && user.getPassword().equals(password)) {
+            request.setAttribute("user", user);
+            request.getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
+        }
+        else {
+            request.setAttribute("error", "Wrong email or password");
+            request.getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
+        }
     }
 }

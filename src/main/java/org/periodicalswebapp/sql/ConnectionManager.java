129f53c57f2379c4289f29996f06dd04155d6f41
vhorielova <horielova.viktoria@gmail.com>
2025-04-26 21:38:35 +0300

    add connection manager to connect to postgres database

---
+++ src/main/java/org/periodicalswebapp/sql/ConnectionManager.java
@@ -0,0 +1,27 @@
+package org.periodicalswebapp.sql;
+
+import org.postgresql.ds.PGSimpleDataSource;
+
+import javax.sql.DataSource;
+
+public class ConnectionManager {
+    private static DataSource dataSource;
+
+    public ConnectionManager() {
+    }
+
+    static {
+        final String url = "jdbc:postgresql://localhost:5432/periodicalswebapp";
+        final PGSimpleDataSource ds = new PGSimpleDataSource();
+        ds.setUrl(url);
+        ds.setUser("postgres");
+        ds.setPassword("");
+        dataSource = ds;
+    }
+
+
+    public static DataSource getDataSource() {
+        System.out.println(dataSource);
+        return dataSource;
+    }
+}

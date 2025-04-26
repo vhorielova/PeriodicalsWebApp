8a54729f2538086ec12345009efdaa5739636e0b
vhorielova <horielova.viktoria@gmail.com>
2025-04-26 21:34:28 +0300

    add UserDao, set session attribute when register and set request attributes

--- src/main/java/org/periodicalswebapp/servlets/handlers/RegisterHandler.java
+++ src/main/java/org/periodicalswebapp/servlets/handlers/RegisterHandler.java
@@ -4,11 +4,16 @@ import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.periodicalswebapp.daoimpl.UserDaoImpl;
 import org.periodicalswebapp.models.User;
 
 import java.io.IOException;
 
 public class RegisterHandler implements Handler{
+
+    private final UserDaoImpl userDao = UserDaoImpl.getInstance();
+
     public void handleGet (HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         System.out.println(request.getRequestURI()+" Get");
 
@@ -28,18 +33,30 @@ public class RegisterHandler implements Handler{
         String password = request.getParameter("password");
         String confirmPassword = request.getParameter("confirmPassword");
 
-        if(name == null || lastname == null || email == null || address == null || index == null || password == null || confirmPassword == null) {
-            request.setAttribute("error", "Please fill all the fields");
+        request.setAttribute("name", name);
+        request.setAttribute("lastname", lastname);
+        request.setAttribute("email", email);
+        request.setAttribute("address", address);
+        request.setAttribute("index", index);
+        request.setAttribute("password", password);
+        request.setAttribute("confirmPassword", confirmPassword);
+
+        if(name.isEmpty() || lastname.isEmpty() || email.isEmpty() || address.isEmpty() || index.isEmpty() || password.isEmpty() || confirmPassword.isEmpty()) {
+            request.setAttribute("error", "Будь ласка, заповніть всі поля");
             handleGet(request, response);
         }
         else if(!password.equals(confirmPassword)) {
-            request.setAttribute("error", "The passwords do not match");
+            request.setAttribute("error", "Пароль не співпадає");
             handleGet(request, response);
         }
         else {
-            User user = new User(name, lastname, email, index, address, index);
+            User user = new User(name, lastname, address, index, email, password);
             System.out.println(user.toString());
-            response.sendRedirect("/WEB-INF/login.jsp");
+            userDao.saveUser(user);
+            HttpSession session = request.getSession();
+            session.setAttribute("user", user);
+            RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/catalog.jsp");
+            dispatcher.forward(request, response);
         }
 
 

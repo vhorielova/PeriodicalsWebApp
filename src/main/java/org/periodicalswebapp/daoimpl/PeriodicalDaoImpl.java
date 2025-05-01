030e635db9ac556bf6be92b0d96fd5189e4b1fae
vhorielova <horielova.viktoria@gmail.com>
2025-05-01 17:15:14 +0300

    implement get periodical by id function

--- src/main/java/org/periodicalswebapp/daoimpl/PeriodicalDaoImpl.java
+++ src/main/java/org/periodicalswebapp/daoimpl/PeriodicalDaoImpl.java
@@ -2,40 +2,92 @@ package org.periodicalswebapp.daoimpl;
 
 import org.periodicalswebapp.dao.PeriodicalDao;
 import org.periodicalswebapp.models.*;
+import org.periodicalswebapp.sql.ConnectionManager;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.ArrayList;
 
 public class PeriodicalDaoImpl implements PeriodicalDao {
-    private List<Periodical> periodicals;
+
+    private static final PeriodicalDaoImpl periodicalDaoImpl = new PeriodicalDaoImpl();
 
     public PeriodicalDaoImpl() {
-        periodicals = new ArrayList<Periodical> ();
-        periodicals.add(new Periodical(1, "NY Times", 30, 50));
-        periodicals.add(new Periodical(2, "The New Yorker", 20, 35));
+    }
+
+    public static PeriodicalDaoImpl getInstance() {
+        return periodicalDaoImpl;
     }
 
     @Override
     public List<Periodical> getAllPeriodicals() {
+        String sql = "select * from periodicals";
+        List<Periodical> periodicals = new ArrayList<>();
+        PreparedStatement stmt = null;
+        Connection conn = null;
+        try {
+            conn = ConnectionManager.getDataSource().getConnection();
+            conn.setAutoCommit(false);
+            stmt = conn.prepareStatement(sql);
+            stmt.executeQuery();
+            conn.commit();
+
+            ResultSet rs = stmt.getResultSet();
+            while(rs.next()) {
+                Periodical periodical = new Periodical();
+                periodical.setId(rs.getInt("id"));
+                periodical.setName(rs.getString("name"));
+                periodical.setHalfYearPrice(rs.getDouble("halfyearprice"));
+                periodical.setFullYearPrice(rs.getDouble("fullyearprice"));
+                periodicals.add(periodical);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Помилка при пошуку періодичних видань", e);
+        }
         return periodicals;
     }
 
     @Override
     public Periodical getPeriodicalById(int id) {
-        for(Periodical periodical : periodicals) {
-            if (periodical.getId() == id) {
+        String sql = "select * from periodicals where id = ?";
+        List<Periodical> periodicals = new ArrayList<>();
+        PreparedStatement stmt = null;
+        Connection conn = null;
+        try {
+            conn = ConnectionManager.getDataSource().getConnection();
+            conn.setAutoCommit(false);
+            stmt = conn.prepareStatement(sql);
+            stmt.setInt(1, id);
+            stmt.executeQuery();
+            conn.commit();
+
+            ResultSet rs = stmt.getResultSet();
+            if(rs.next()) {
+                Periodical periodical = new Periodical();
+                periodical.setId(rs.getInt("id"));
+                periodical.setName(rs.getString("name"));
+                periodical.setType(rs.getString("type"));
+                periodical.setHalfYearPrice(rs.getDouble("halfyearprice"));
+                periodical.setFullYearPrice(rs.getDouble("fullyearprice"));
+                periodicals.add(periodical);
                 return periodical;
             }
+        } catch (SQLException e) {
+            throw new RuntimeException("Помилка при пошуку періодичних видань", e);
         }
         return null;
     }
 
     @Override
     public void savePeriodical(Periodical periodical) {
-        periodicals.add(periodical);
+
     }
 
     @Override
     public void deletePeriodical(Periodical periodical) {
-        periodicals.remove(periodical);
+
     }
 }

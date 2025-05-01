543f40999e303ebcf45103fb3e117dc448baa146
vhorielova <horielova.viktoria@gmail.com>
2025-05-01 17:14:50 +0300

    add periodicalCart attribute to session

--- src/main/java/org/periodicalswebapp/servlets/handlers/CatalogHandler.java
+++ src/main/java/org/periodicalswebapp/servlets/handlers/CatalogHandler.java
@@ -4,10 +4,12 @@ import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import org.periodicalswebapp.daoimpl.PeriodicalDaoImpl;
 import org.periodicalswebapp.models.Periodical;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CatalogHandler implements Handler {
@@ -16,12 +18,26 @@ public class CatalogHandler implements Handler {
         List<Periodical> periodicals = periodicalDao.getAllPeriodicals();
         System.out.println(periodicals);
         request.setAttribute("periodicals", periodicals);
+
+        HttpSession session = request.getSession();
+        List <Periodical> periodicalsCart = session.getAttribute("periodicalsCart") == null ? new ArrayList<>() : (List<Periodical>) session.getAttribute("periodicalsCart");
+        request.setAttribute("periodicalsCart", periodicalsCart);
+
         RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/catalog.jsp");
         dispatcher.forward(request, response);
     }
 
     public void handlePost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        HttpSession session = request.getSession();
+        List <Periodical> periodicalsCart = session.getAttribute("periodicalsCart") == null ? new ArrayList<>() : (List<Periodical>) session.getAttribute("periodicalsCart");
 
-        RequestDispatcher dispatcher = request.getRequestDispatcher("/catalog");
+        PeriodicalDaoImpl periodicalDao = new PeriodicalDaoImpl();
+        int periodicalId = Integer.parseInt(request.getParameter("periodicalId"));
+        Periodical periodical = periodicalDao.getPeriodicalById(periodicalId);
+
+        periodicalsCart.add(periodical);
+
+        session.setAttribute("periodicalsCart", periodicalsCart);
+        handleGet(request, response);
     }
 }

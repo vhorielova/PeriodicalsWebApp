f2c575b73e358b5d5113787c3a8bfe8def159260
vhorielova <horielova.viktoria@gmail.com>
2025-05-02 13:14:18 +0300

    implement delete from cart

--- src/main/java/org/periodicalswebapp/servlets/handlers/CartHandler.java
+++ src/main/java/org/periodicalswebapp/servlets/handlers/CartHandler.java
@@ -3,8 +3,14 @@ package org.periodicalswebapp.servlets.handlers;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.periodicalswebapp.daoimpl.PeriodicalDaoImpl;
+import org.periodicalswebapp.models.Periodical;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class CartHandler implements Handler {
     public void handleGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
@@ -12,6 +18,20 @@ public class CartHandler implements Handler {
     }
 
     public void handlePost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if(request.getParameter("delete") != null && request.getParameter("delete").equals("true")) {
+            HttpSession session = request.getSession();
+            List<Periodical> periodicalsCart = session.getAttribute("periodicalsCart") == null ? new ArrayList<>() : (List<Periodical>) session.getAttribute("periodicalsCart");
 
+            PeriodicalDaoImpl periodicalDao = new PeriodicalDaoImpl();
+            int periodicalId = Integer.parseInt(request.getParameter("periodicalId"));
+
+//            periodicalsCart.remove(periodical);
+            periodicalsCart = periodicalsCart.stream().filter(x -> x.getId() != periodicalId).collect(Collectors.toList());
+
+            session.setAttribute("periodicalsCart", periodicalsCart);
+            handleGet(request, response);
+        }
+        else{
+        }
     }
 }

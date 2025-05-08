ba8012ce6661e547f89366fc7a1797e14818ffdb
vhorielova <horielova.viktoria@gmail.com>
2025-05-08 18:10:50 +0300

    add options for 6 and 12 months

--- src/main/java/org/periodicalswebapp/servlets/handlers/CatalogHandler.java
+++ src/main/java/org/periodicalswebapp/servlets/handlers/CatalogHandler.java
@@ -5,38 +5,80 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import jakarta.ws.rs.core.AbstractMultivaluedMap;
 import org.periodicalswebapp.daoimpl.PeriodicalDaoImpl;
+import org.periodicalswebapp.daoimpl.SubscriptionDaoImpl;
 import org.periodicalswebapp.models.Periodical;
+import org.periodicalswebapp.models.Subscription;
+import org.periodicalswebapp.models.User;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class CatalogHandler implements Handler {
+    PeriodicalDaoImpl periodicalDao = new PeriodicalDaoImpl();
+    SubscriptionDaoImpl subscriptionDao = new SubscriptionDaoImpl();
     public void handleGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        PeriodicalDaoImpl periodicalDao = new PeriodicalDaoImpl();
+
         List<Periodical> periodicals = periodicalDao.getAllPeriodicals();
         request.setAttribute("periodicals", periodicals);
 
         HttpSession session = request.getSession();
-        List <Periodical> periodicalsCart = session.getAttribute("periodicalsCart") == null ? new ArrayList<>() : (List<Periodical>) session.getAttribute("periodicalsCart");
+        HashMap <Periodical, Integer> periodicalsCart = session.getAttribute("periodicalsCart") == null ? new HashMap<>() : (HashMap<Periodical, Integer>) session.getAttribute("periodicalsCart");
         request.setAttribute("periodicalsCart", periodicalsCart);
+
+        subscriptionDao.update();
+        //List <Periodical> order = session.getAttribute("order") == null ? new ArrayList<>() : (List<Periodical>) session.getAttribute("order");
+        List <Subscription> subscriptions = subscriptionDao.getAllUserSubscriptions(((User) session.getAttribute("user")).getId());
+        request.setAttribute("subscriptions", subscriptions);
 
         RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/catalog.jsp");
         dispatcher.forward(request, response);
     }
 
     public void handlePost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
-        HttpSession session = request.getSession();
-        List <Periodical> periodicalsCart = session.getAttribute("periodicalsCart") == null ? new ArrayList<>() : (List<Periodical>) session.getAttribute("periodicalsCart");
+        String addToCart = request.getParameter("periodicalId");
+        String delete = request.getParameter("deletePeriodical");
+        String add = request.getParameter("addPeriodical");
 
-        PeriodicalDaoImpl periodicalDao = new PeriodicalDaoImpl();
-        int periodicalId = Integer.parseInt(request.getParameter("periodicalId"));
-        Periodical periodical = periodicalDao.getPeriodicalById(periodicalId);
+        if(addToCart != null) {
+            HttpSession session = request.getSession();
+            HashMap <Periodical, Integer> periodicalsCart = session.getAttribute("periodicalsCart") == null ? new HashMap<>() : (HashMap<Periodical, Integer>) session.getAttribute("periodicalsCart");
 
-        periodicalsCart.add(periodical);
+            PeriodicalDaoImpl periodicalDao = new PeriodicalDaoImpl();
+            int periodicalId = Integer.parseInt(request.getParameter("periodicalId"));
+            Periodical periodical = periodicalDao.getPeriodicalById(periodicalId);
 
-        session.setAttribute("periodicalsCart", periodicalsCart);
+            int period = Integer.parseInt(request.getParameter("subscriptionPeriod"));
+
+            periodicalsCart.put(periodical, period);
+
+            session.setAttribute("periodicalsCart", periodicalsCart);
+        }
+        else if(delete != null) {
+            PeriodicalDaoImpl periodicalDao = new PeriodicalDaoImpl();
+            periodicalDao.deletePeriodical(Integer.parseInt(delete));
+        }
+        else if(add != null) {
+            PeriodicalDaoImpl periodicalDao = new PeriodicalDaoImpl();
+
+            String name = request.getParameter("name");
+
+            if(name.isEmpty() || request.getParameter("halfYearPrice").isEmpty() || request.getParameter("fullYearPrice").isEmpty()){
+                request.setAttribute("error", "Заповніть всі поля");
+                handleGet(request, response);
+            }
+            double halfYearPrice = Double.parseDouble(request.getParameter("halfYearPrice"));
+            double fullYearPrice = Double.parseDouble(request.getParameter("fullYearPrice"));
+
+            Periodical periodical = new Periodical(name, halfYearPrice, fullYearPrice);
+            periodicalDao.savePeriodical(periodical);
+        }
+
+
         handleGet(request, response);
     }
 }

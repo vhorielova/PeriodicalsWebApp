b894be962557782dc0f05e3a3f9f71ab8b718ad7
vhorielova <horielova.viktoria@gmail.com>
2025-05-12 01:54:43 +0300

    delete unnecessary import

--- src/main/java/org/periodicalswebapp/servlets/handlers/Handler.java
+++ src/main/java/org/periodicalswebapp/servlets/handlers/Handler.java
@@ -4,8 +4,6 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
 
 import java.io.IOException;
-import java.sql.SQLException;
-
 
 public interface Handler {
     void handleGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException;

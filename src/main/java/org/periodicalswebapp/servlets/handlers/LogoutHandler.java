9a55824e497692374f96f73dacc9c0678d5dc2d7
vhorielova <horielova.viktoria@gmail.com>
2025-05-12 01:58:01 +0300

    add logging

--- src/main/java/org/periodicalswebapp/servlets/handlers/LogoutHandler.java
+++ src/main/java/org/periodicalswebapp/servlets/handlers/LogoutHandler.java
@@ -3,16 +3,23 @@ package org.periodicalswebapp.servlets.handlers;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
 public class LogoutHandler implements Handler {
-    @Override
+    Logger logger = LoggerFactory.getLogger(LogoutHandler.class);
+
     public void handleGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        logger.info("Handling GET request for logout");
+        logger.debug("Invalidating session.");
         request.getSession().invalidate();
+        logger.debug("Redirecting to main page after logout.");
         response.sendRedirect("/");
     }
 
     public void handlePost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        logger.info("Handling POST request for logout");
     }
 }
